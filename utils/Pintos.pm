e90a3cab93f4bf41030d2e774f98f6ec6fcf1cba
Nathan <nfaulkner@utexas.edu>
2016-11-22 14:39:04 -0600

    initial commit for project 5

--- utils/Pintos.pm
+++ utils/Pintos.pm
@@ -360,7 +360,7 @@ sub cyl_sectors {
 sub read_loader {
     my ($name) = @_;
     ###--- Change the string in quotes below to your location of loader.bin---### 
-    $name = find_file ("/home/nfaulkner/OS_Stuff/project2/userprog/build/loader.bin") if !defined $name;
+    $name = find_file ("/home/nfaulkner/OS_Stuff/project5/filesys/build/loader.bin") if !defined $name;
     die "Cannot find loader\n" if !defined $name;
 
     my ($handle);
